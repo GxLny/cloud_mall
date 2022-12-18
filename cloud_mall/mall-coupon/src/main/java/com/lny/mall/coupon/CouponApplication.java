4ab1c2d9a41e736462f399b57339d64be8c44f40
lny <lny@163.com>
2022-12-18 21:59:13 +0800

    测试远程调用服务，和nacos注册中心和配置中心

--- cloud_mall/mall-coupon/src/main/java/com/lny/mall/coupon/CouponApplication.java
+++ cloud_mall/mall-coupon/src/main/java/com/lny/mall/coupon/CouponApplication.java
@@ -3,9 +3,12 @@ package com.lny.mall.coupon;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @MapperScan("com.lny.mall.coupon.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CouponApplication {
     public static void main(String[] args) {
         SpringApplication.run(CouponApplication.class,args);
--- cloud_mall/mall-coupon/src/main/java/com/lny/mall/coupon/controller/CouponController.java
+++ cloud_mall/mall-coupon/src/main/java/com/lny/mall/coupon/controller/CouponController.java
@@ -4,11 +4,9 @@ import java.util.Arrays;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.*;
 
 import com.lny.mall.coupon.entity.CouponEntity;
 import com.lny.mall.coupon.service.CouponService;
@@ -25,10 +23,39 @@ import com.lny.common.utils.R;
  * @date 2022-12-16 11:54:42
  */
 @RestController
+@RefreshScope //开启自动刷新配置信息
 @RequestMapping("coupon/coupon")
 public class CouponController {
     @Autowired
     private CouponService couponService;
+
+    @Value("${server.port}")
+    private String serverPort;
+
+    /**
+     * 引入依赖
+     * 创建bootstarp文件，添加服务器名称，nacos服务地址
+     * @RefreshScope 动态获取配置
+     * 如果配置中心和当前应用都配置了相同的属性，则优先使用配置中心的
+     * @return
+     */
+    @GetMapping("/serverPort")
+    public R getNacosConfig(){
+
+        return R.ok(serverPort);
+    }
+
+
+    /**
+     * 测试远程调用接口
+     * @return
+     */
+    @RequestMapping("/feign/list")
+    public R feignList(){
+        CouponEntity couponEntity = new CouponEntity();
+        couponEntity.setCouponName("满10减3！！");
+        return R.ok().put("coupon",couponEntity);
+    }
 
     /**
      * 列表
