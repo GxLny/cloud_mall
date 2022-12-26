904f28ec6c6bd7bbf6d120aa61dff3ec0598412d
lny <lny@163.com>
2022-12-26 17:38:54 +0800

    前端：新增页面

--- renren-fast-vue/src/views/modules/product/brand-add-or-update.vue
+++ renren-fast-vue/src/views/modules/product/brand-add-or-update.vue
@@ -1,7 +1,16 @@
 <template>
-  <el-dialog :title="!dataForm.brandId ? '新增' : '修改'" :close-on-click-modal="false" :visible.sync="visible">
-    <el-form :model="dataForm" :rules="dataRule" ref="dataForm" @keyup.enter.native="dataFormSubmit()"
-      label-width="30%">
+  <el-dialog
+    :title="!dataForm.id ? '新增' : '修改'"
+    :close-on-click-modal="false"
+    :visible.sync="visible"
+  >
+    <el-form
+      :model="dataForm"
+      :rules="dataRule"
+      ref="dataForm"
+      @keyup.enter.native="dataFormSubmit()"
+      label-width="140px"
+    >
       <el-form-item label="品牌名" prop="name">
         <el-input v-model="dataForm.name" placeholder="品牌名"></el-input>
       </el-form-item>
@@ -13,12 +22,13 @@
         <el-input v-model="dataForm.descript" placeholder="介绍"></el-input>
       </el-form-item>
       <el-form-item label="显示状态" prop="showStatus">
-        <!-- <el-input v-model="dataForm.showStatus" placeholder="显示状态"></el-input> -->
-        <template>
-          <el-switch v-model="dataForm.showStatus" active-color="#13ce66" inactive-color="#ff4949" :active-value="1"
-            :inactive-value="0">
-          </el-switch>
-        </template>
+        <el-switch
+          v-model="dataForm.showStatus"
+          active-color="#13ce66"
+          inactive-color="#ff4949"
+          :active-value="1"
+          :inactive-value="0"
+        ></el-switch>
       </el-form-item>
       <el-form-item label="检索首字母" prop="firstLetter">
         <el-input v-model="dataForm.firstLetter" placeholder="检索首字母"></el-input>
@@ -35,11 +45,9 @@
 </template>
 
 <script>
-import singleUpload from '@/components/upload/singleUpload';
+import SingleUpload from "@/components/upload/singleUpload";
 export default {
-  components: {
-    singleUpload
-  },
+  components: { SingleUpload },
   data() {
     return {
       visible: false,
@@ -48,33 +56,32 @@ export default {
         name: "",
         logo: "",
         descript: "",
-        showStatus: "",
+        showStatus: 1,
         firstLetter: "",
-        sort: "",
+        sort: 0
       },
       dataRule: {
         name: [{ required: true, message: "品牌名不能为空", trigger: "blur" }],
         logo: [
-          { required: true, message: "品牌logo地址不能为空", trigger: "blur" },
+          { required: true, message: "品牌logo地址不能为空", trigger: "blur" }
         ],
         descript: [
-          { required: true, message: "介绍不能为空", trigger: "blur" },
+          { required: true, message: "介绍不能为空", trigger: "blur" }
         ],
         showStatus: [
           {
             required: true,
             message: "显示状态[0-不显示；1-显示]不能为空",
-            trigger: "blur",
-          },
+            trigger: "blur"
+          }
         ],
         firstLetter: [
           {
-            // 自定义校验器
             validator: (rule, value, callback) => {
               if (value == "") {
                 callback(new Error("首字母必须填写"));
               } else if (!/^[a-zA-Z]$/.test(value)) {
-                callback(new Error("首字母必须a-z或者A-Z之间且只有一个字母"));
+                callback(new Error("首字母必须a-z或者A-Z之间"));
               } else {
                 callback();
               }
@@ -82,19 +89,21 @@ export default {
             trigger: "blur"
           }
         ],
-        sort: [{
-          validator: (rule, value, callback) => {
-            if (value == "") {
-              callback(new Error("排序字段必须填写"));
-            } else if (!Number.isInteger(value) || value < 0) {
-              callback(new Error("排序必须是一个大于等于0的整数"));
-            } else {
-              callback();
-            }
-          },
-          trigger: "blur"
-        }],
-      },
+        sort: [
+          {
+            validator: (rule, value, callback) => {
+              if (value == "") {
+                callback(new Error("排序字段必须填写"));
+              } else if (!Number.isInteger(value) || value<0) {
+                callback(new Error("排序必须是一个大于等于0的整数"));
+              } else {
+                callback();
+              }
+            },
+            trigger: "blur"
+          }
+        ]
+      }
     };
   },
   methods: {
@@ -109,7 +118,7 @@ export default {
               `/product/brand/info/${this.dataForm.brandId}`
             ),
             method: "get",
-            params: this.$http.adornParams(),
+            params: this.$http.adornParams()
           }).then(({ data }) => {
             if (data && data.code === 0) {
               this.dataForm.name = data.brand.name;
@@ -125,7 +134,7 @@ export default {
     },
     // 表单提交
     dataFormSubmit() {
-      this.$refs["dataForm"].validate((valid) => {
+      this.$refs["dataForm"].validate(valid => {
         if (valid) {
           this.$http({
             url: this.$http.adornUrl(
@@ -139,8 +148,8 @@ export default {
               descript: this.dataForm.descript,
               showStatus: this.dataForm.showStatus,
               firstLetter: this.dataForm.firstLetter,
-              sort: this.dataForm.sort,
-            }),
+              sort: this.dataForm.sort
+            })
           }).then(({ data }) => {
             if (data && data.code === 0) {
               this.$message({
@@ -150,7 +159,7 @@ export default {
                 onClose: () => {
                   this.visible = false;
                   this.$emit("refreshDataList");
-                },
+                }
               });
             } else {
               this.$message.error(data.msg);
@@ -158,7 +167,7 @@ export default {
           });
         }
       });
-    },
-  },
+    }
+  }
 };
 </script>
